a95f18495f1a63ca09509410dc45c351c4293b70
binishjoshi <beeneez6233@gmail.com>
2019-10-31 12:54:15 +0545

    Added React Router DOM package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>,
+    document.getElementById('root')
+);
