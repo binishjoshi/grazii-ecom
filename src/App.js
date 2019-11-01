8e2f836464ad893ceea116aababab46d00b810f5
binishjoshi <beeneez6233@gmail.com>
2019-11-02 05:10:41 +0545

    Added shop Route and added components and layouts for shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
