8c8bf4141409e85484451aec2ee2d067131b5ab6
binishjoshi <beeneez6233@gmail.com>
2019-11-03 05:48:55 +0545

    Added Sign-in Page and CustomButton and FormInput

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUp} />
     </div>
   );
 }
