7962b76ff79789cdf6ab585cfde1d0924983cc91
Binish Joshi <beeneez6233@gmail.com>
2019-11-13 14:57:10 +0545

    Windows, and sign up component

--- src/App.js
+++ src/App.js
@@ -32,8 +32,11 @@ class App extends React.Component {
               ...snapShot.data()
             }
           })
+
+          console.log(this.state);
         })
       }
+      
       else {
         this.setState({currentUser: userAuth});
       }
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,14 +4,14 @@ import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className='group'>
-        <input className='form-input' onChange={handleChange} {...otherProps} />
-        {
+        <input placeholder={label} className='form-input' onChange={handleChange} {...otherProps} />
+        {/* {
             label ? 
             (<label className={`${otherProps.value.length} ? 'shrink': '' form-input-label`}>
                 {label}
             </label>)
             : null
-        }
+        } */}
     </div>
 )
 
