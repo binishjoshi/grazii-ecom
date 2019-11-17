4d304866c3fbef0bc503fb2749b1c0c11625b161
Binish Joshi <beeneez6233@gmail.com>
2019-11-17 11:19:15 +0545

    Added redirect for signed-in user to restrict from using sign-in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -45,14 +45,21 @@ class App extends React.Component {
         <Header />
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
+        <Route path='/signin' render={() => 
+          this.props.currentUser ? 
+          (<Redirect to='/' />) : 
+          (<SignInAndSignUp />)} />
       </div>
     );
   }
 }
 
+const mapStateToProps = (({user}) => ({
+  currentUser: user.currentUser
+}))
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
