15336966c5993d99c71abb34231133631d1f88df
Binish Joshi <beeneez6233@gmail.com>
2019-11-29 09:25:43 +0545

    Added quantity in CartDropDown state along with uniquid for id's

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 const SHOP_DATA = [
     {
       id: 1,
@@ -5,55 +7,55 @@ const SHOP_DATA = [
       routeName: 'hats',
       items: [
         {
-          id: 1,
+          id: uniqid(),
           name: 'Brown Brim',
           imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
           price: 25
         },
         {
-          id: 2,
+          id: uniqid(),
           name: 'Blue Beanie',
           imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
           price: 18
         },
         {
-          id: 3,
+          id: uniqid(),
           name: 'Brown Cowboy',
           imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
           price: 35
         },
         {
-          id: 4,
+          id: uniqid(),
           name: 'Grey Brim',
           imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
           price: 25
         },
         {
-          id: 5,
+          id: uniqid(),
           name: 'Green Beanie',
           imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
           price: 18
         },
         {
-          id: 6,
+          id: uniqid(),
           name: 'Palm Tree Cap',
           imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
           price: 14
         },
         {
-          id: 7,
+          id: uniqid(),
           name: 'Red Beanie',
           imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
           price: 18
         },
         {
-          id: 8,
+          id: uniqid(),
           name: 'Wolf Cap',
           imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
           price: 14
         },
         {
-          id: 9,
+          id: uniqid(),
           name: 'Blue Snapback',
           imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
           price: 16
@@ -66,49 +68,49 @@ const SHOP_DATA = [
       routeName: 'sneakers',
       items: [
         {
-          id: 10,
+          id: uniqid(),
           name: 'Adidas NMD',
           imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
           price: 220
         },
         {
-          id: 11,
+          id: uniqid(),
           name: 'Adidas Yeezy',
           imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
           price: 280
         },
         {
-          id: 12,
+          id: uniqid(),
           name: 'Black Converse',
           imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
           price: 110
         },
         {
-          id: 13,
+          id: uniqid(),
           name: 'Nike White AirForce',
           imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
           price: 160
         },
         {
-          id: 14,
+          id: uniqid(),
           name: 'Nike Red High Tops',
           imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
           price: 160
         },
         {
-          id: 15,
+          id: uniqid(),
           name: 'Nike Brown High Tops',
           imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
           price: 160
         },
         {
-          id: 16,
+          id: uniqid(),
           name: 'Air Jordan Limited',
           imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
           price: 190
         },
         {
-          id: 17,
+          id: uniqid(),
           name: 'Timberlands',
           imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
           price: 200
@@ -121,31 +123,31 @@ const SHOP_DATA = [
       routeName: 'jackets',
       items: [
         {
-          id: 18,
+          id: uniqid(),
           name: 'Black Jean Shearling',
           imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
           price: 125
         },
         {
-          id: 19,
+          id: uniqid(),
           name: 'Blue Jean Jacket',
           imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
           price: 90
         },
         {
-          id: 20,
+          id: uniqid(),
           name: 'Grey Jean Jacket',
           imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
           price: 90
         },
         {
-          id: 21,
+          id: uniqid(),
           name: 'Brown Shearling',
           imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
           price: 165
         },
         {
-          id: 22,
+          id: uniqid(),
           name: 'Tan Trench',
           imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
           price: 185
@@ -158,43 +160,43 @@ const SHOP_DATA = [
       routeName: 'womens',
       items: [
         {
-          id: 23,
+          id: uniqid(),
           name: 'Blue Tanktop',
           imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-          price: 25
+          price: uniqid()
         },
         {
-          id: 24,
+          id: uniqid(),
           name: 'Floral Blouse',
           imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
           price: 20
         },
         {
-          id: 25,
+          id: uniqid(),
           name: 'Floral Dress',
           imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
           price: 80
         },
         {
-          id: 26,
+          id: uniqid(),
           name: 'Red Dots Dress',
           imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
           price: 80
         },
         {
-          id: 27,
+          id: uniqid(),
           name: 'Striped Sweater',
           imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
           price: 45
         },
         {
-          id: 28,
+          id: uniqid(),
           name: 'Yellow Track Suit',
           imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
           price: 135
         },
         {
-          id: 29,
+          id: uniqid(),
           name: 'White Blouse',
           imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
           price: 20
@@ -207,37 +209,37 @@ const SHOP_DATA = [
       routeName: 'mens',
       items: [
         {
-          id: 30,
+          id: uniqid(),
           name: 'Camo Down Vest',
           imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
           price: 325
         },
         {
-          id: 31,
+          id: uniqid(),
           name: 'Floral T-shirt',
           imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
           price: 20
         },
         {
-          id: 32,
+          id: uniqid(),
           name: 'Black & White Longsleeve',
           imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
           price: 25
         },
         {
-          id: 33,
+          id: uniqid(),
           name: 'Pink T-shirt',
           imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
           price: 25
         },
         {
-          id: 34,
+          id: uniqid(),
           name: 'Jean Long Sleeve',
           imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
           price: 40
         },
         {
-          id: 35,
+          id: uniqid(),
           name: 'Burgundy T-shirt',
           imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
           price: 25
