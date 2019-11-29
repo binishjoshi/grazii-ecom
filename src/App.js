948301250946aba9332abe0ff5e5e31ee42799c3
Binish Joshi <beeneez6233@gmail.com>
2019-11-29 15:43:24 +0545

    Added User Selector and replaced all mapStateToProps by selectors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -8,6 +9,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';  
 
 import './App.css';
 
@@ -54,9 +56,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = (({user}) => ({
-  currentUser: user.currentUser
-}))
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
