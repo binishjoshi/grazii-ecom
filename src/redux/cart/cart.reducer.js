192c044a6a39e57c472a2b0ed2cacfff9e2af967
Binish Joshi <beeneez6233@gmail.com>
2019-11-29 10:02:57 +0545

    Added Cart Items Functionality on Cart Dropdown

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -9,7 +9,6 @@ const INITIAL_STATE = {
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
-            console.log('okay');
             return {
                 ...state, 
                 hidden: !state.hidden
