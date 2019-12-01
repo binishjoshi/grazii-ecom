44a71bc54f41e379f29465f7d85495d3ef06acb7
Binish Joshi <beeneez6233@gmail.com>
2019-12-01 06:14:17 +0545

    Advanved routing for shop and array replaced by object

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
+import CollectionPage from '../../pages/collection/collection.component';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({match}) => (
     <div className='shop-page'>
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
