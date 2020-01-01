0ae4f6b85bf0fe7b6da7763890fa272fb477bfb2
Binish Joshi <beeneez6233@gmail.com>
2020-01-01 02:37:16 -0500

    Added fetched data to reducers

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -64,7 +64,11 @@ export const convertCollectionsSnapshotToMap = (collections) => {
       items
     };
   });
-  console.log(transformedCollection);
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {})
 };
 
 export const auth = firebase.auth();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionPage from '../../pages/collection/collection.component';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+import { updateCollections } from '../../redux/shop/shop.actions';
+
 class ShopPage extends React.Component {
 	unsubscribeFromSnapshot = null;
 
 	componentDidMount() {
+		const { updateCollections } = this.props;
 		const collectionRef = firestore.collection('collections');
 
 		collectionRef.onSnapshot(async snapshot => {
-			convertCollectionsSnapshotToMap(snapshot);
+			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+			updateCollections(collectionsMap);
 		})
 	}
 
@@ -28,4 +33,8 @@ class ShopPage extends React.Component {
 	}
 } 
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+	updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage);
