23c53e83bd0d828a073efe7252845ce71dff2c6c
Binish Joshi <beeneez6233@gmail.com>
2020-01-01 02:05:38 -0500

    Fetched shop data from firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,28 @@ import { Route } from 'react-router-dom';
 import CollectionPage from '../../pages/collection/collection.component';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-const ShopPage = ({match}) => (
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-);
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+class ShopPage extends React.Component {
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const collectionRef = firestore.collection('collections');
+
+		collectionRef.onSnapshot(async snapshot => {
+			convertCollectionsSnapshotToMap(snapshot);
+		})
+	}
+
+	render() {
+		const { match } = this.props;
+		return (
+	    <div className='shop-page'>
+	        <Route exact path={`${match.path}`} component={CollectionOverview} />
+	        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+	    </div>
+		);
+	}
+} 
 
 export default ShopPage;
