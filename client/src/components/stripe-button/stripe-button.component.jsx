98eec8464e82bb10ee29dc7b542d093ee1739827
Binish Joshi <beeneez6233@gmail.com>
2020-01-17 21:05:08 -0500

    Connected stripe frontend to backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_kXvet59wJyT1RTkvddLSjStX00QYXdIBJ3';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+        .then(response => {
+            alert('Payment Successful')
+        })
+        .catch(error => {
+            console.log('Payment error', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card');
+        })
     };
 
     return (
